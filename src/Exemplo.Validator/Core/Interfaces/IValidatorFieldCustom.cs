ce10610cebb02cab2ffffdd6e25eabb77b98e308
leandro silveira <leandrobhmgg@gmail.com>
2019-07-19 21:20:06 -0300

    commit files

---
+++ src/Exemplo.Validator/Core/Interfaces/IValidatorFieldCustom.cs
@@ -0,0 +1,13 @@
+﻿using KellermanSoftware.CompareNetObjects;
+
+namespace Exemplo.Validator.Interfaces
+{
+    public interface IValidatorFieldCustom<T>
+    {
+        string Name
+        {
+            get;
+        }
+        ValidationItem Valid(T item, Difference diference);
+    }
+}
---
+++ src/Exemplo.Validator/Example/ValidatorProcesso/Fields/ValidatorItemVara.cs
@@ -0,0 +1,28 @@
+﻿using Exemplo.Validator.Interfaces;
+using Exemplo.Validator.Services;
+using KellermanSoftware.CompareNetObjects;
+
+namespace Exemplo.Validator.ValidatorProcesso.Fields
+{
+    public class ValidatorItemVara : IValidatorFieldCustom<Processo>
+    {
+        public string Name => nameof(Processo.VaraId);
+
+        private readonly IVaraService _varaService;
+
+        public ValidatorItemVara(IVaraService varaService)
+        {
+            _varaService = varaService;
+        }
+        public ValidationItem Valid(Processo item, Difference diference)
+        {
+            var vara = _varaService.GetById(item.VaraId.Value);
+
+            if (vara == default)
+                return new ValidationItem(Name, string.Format("Vara não existe {0}", item.VaraId.Value));
+
+            else
+                return null;
+        }
+    }
+}
